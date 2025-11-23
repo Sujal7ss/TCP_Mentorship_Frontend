a7df16ceb4dffaba7cb17967792e459e0b0f136b
Sujal7ss <sujalchahande3@gmail.com>
2025-11-23 13:54:53 +0530

    Updated base url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,2 +1,2 @@
-export const base_url = "https://codeutsava.nitrr.ac.in/server/";
+export const base_url = import.meta.env.VITE_BASE_URL || "https://codeutsava.nitrr.ac.in/server/";
 // export const base_url = "http://127.0.0.1:8000/server/";
